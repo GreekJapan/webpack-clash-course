45587459583e58aa4c5825cdf9d87e505787f6ad
Greek Japan <greekja35678@gmail.com>
2020-05-09 14:49:45 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import _ from 'lodash';
+import * as utilities from './utilities';
+import Lion from './utilities';
+
+console.log(utilities.universityName);
+console.log(utilities.niJou(100));
+console.log(Lion.say());
+
 const component = () => {
   const element = document.createElement('div');
   const array = ['Hello', 'webpack', '!!!'];
